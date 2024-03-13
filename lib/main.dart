96e775e96413338cc25da6ec81b253e9c0b5a1d4
om1007 <117250624+om1007@users.noreply.github.com>
2024-03-14 01:44:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:provider/provider.dart';
 import 'screens/StudentLoginPage.dart';
 import 'screens/StudentSignupPage.dart';
@@ -6,16 +8,15 @@ import 'screens/FacultyLoginPage.dart';
 import 'screens/FacultySignupPage.dart';
 import 'screens/Settings.dart'; // Import the SettingsPage.dart file
 import 'screens/AboutPage.dart'; // Import the AboutPage.dart file
-import 'package:firebase_core/firebase_core.dart';
-import 'fire_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'fire_options.dart';
 
 bool _darkThemeEnabled = true;
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async{
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeProvider(), // Provide your ThemeProvider instance here
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'IT Live',
       theme: _darkThemeEnabled ? ThemeData.dark() : ThemeData.light(),
       darkTheme: ThemeData.dark(),
