2ad07d43d62d32781c9a40920c9b8137f34a91e5
om1007 <117250624+om1007@users.noreply.github.com>
2024-03-14 02:56:21 +0530

    Update FacultyLoginPage.dart

--- lib/screens/FacultyLoginPage.dart
+++ lib/screens/FacultyLoginPage.dart
@@ -1,27 +1,36 @@
 // lib/screens/FacultyLoginPage.dart
 
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'FacultySignupPage.dart';
+// import 'FacultyHomePage.dart';
+
+//import 'dart:js';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'FacultySignupPage.dart';
 import 'FacultyHomePage.dart';
 
+
+// import 'StudentSignupPage.dart'; // Import the corresponding signup page
+// import 'StudentHomePage.dart'; // Import the student home page
+//
 class FacultyLoginPage extends StatelessWidget {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  Future<void> signInWithEmailAndPassword() async {
-    try {
-      await Auth().signInWithEmailAndPassword(
-        email: _controllerEmail.text,
-        passwoed: _controllerPassword.text,
-      );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
+  // signIn()async{
+  //   await FirebaseAuth.instance.signInWithEmailAndPassword(email: _emailController.text, password: _passwordController.text);
+  // }
 
-    }
+  signIn() async{
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: _emailController.text, password: _passwordController.text);
+
   }
+
+
 
   FacultyLoginPage({super.key});
 
@@ -36,6 +45,9 @@ class FacultyLoginPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            // Add your student login form widgets here
+
+            // Example: TextFields for email and password
             TextFormField(
               controller: _emailController,
               decoration: const InputDecoration(labelText: 'Email'),
@@ -45,14 +57,24 @@ class FacultyLoginPage extends StatelessWidget {
               decoration: const InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
+
             const SizedBox(height: 16.0),
+
             ElevatedButton(
-              onPressed: () {
-                _performLogin(context);
-              },
+              onPressed: (()=>signIn()),
+              //   // Implement student login logic here
+              //   _performLogin(context);
               child: const Text('Login'),
             ),
+
+            // ElevatedButton(
+            //     onPressed: (()=>signIn()) => _performLogin(context), // Call the function correctly
+            //   child: const Text('Login'),
+            // ),
+
+
             const SizedBox(height: 16.0),
+
             TextButton(
               onPressed: () {
                 Navigator.push(
@@ -91,18 +113,14 @@ class FacultyLoginPage extends StatelessWidget {
       return;
     }
 
-    // Navigate to the faculty home page after successful login
+    // Navigate to the student home page after successful login
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) {
-          // ignore: prefer_const_constructors
-          return FacultyHomePage(facultyName: 'Faculty');
-        }, // Pass faculty's name here
+        builder: (context) => const FacultyHomePage(facultyName: 'Faculty'), // Pass student's name here
       ),
     );
   }
-
 
   void _showAlertDialog(BuildContext context, String message) {
     showDialog(
@@ -124,3 +142,128 @@ class FacultyLoginPage extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+// class FacultyLoginPage extends StatelessWidget {
+//   // final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//
+//   // Future<void> signInWithEmailAndPassword() async {
+//   //   try {
+//   //     await Auth().signInWithEmailAndPassword(
+//   //       email: _controllerEmail.text,
+//   //       passwoed: _controllerPassword.text,
+//   //     );
+//   //   } on FirebaseAuthException catch (e) {
+//   //     setState(() {
+//   //       errorMessage = e.message;
+//   //     });
+//   //
+//   //   }
+//   // }
+//
+//   FacultyLoginPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Faculty Login'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             TextFormField(
+//               controller: _emailController,
+//               decoration: const InputDecoration(labelText: 'Email'),
+//             ),
+//             TextFormField(
+//               controller: _passwordController,
+//               decoration: const InputDecoration(labelText: 'Password'),
+//               obscureText: true,
+//             ),
+//             const SizedBox(height: 16.0),
+//             ElevatedButton(
+//               onPressed: () {
+//                 _performLogin(context);
+//               },
+//               child: const Text('Login'),
+//             ),
+//             const SizedBox(height: 16.0),
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => FacultySignupPage()),
+//                 );
+//               },
+//               child: const Text('Don\'t have an account? Sign up'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   void _performLogin(BuildContext context) {
+//     // Hardcoded email and password for demonstration
+//     String hardcodedEmail = 'faculty@gmail.com';
+//     String hardcodedPassword = 'faculty';
+//
+//     String enteredEmail = _emailController.text.trim();
+//     String enteredPassword = _passwordController.text.trim();
+//
+//     if (enteredEmail.isEmpty || enteredPassword.isEmpty) {
+//       _showAlertDialog(context, 'Fields cannot be empty.');
+//       return;
+//     }
+//
+//     if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(enteredEmail)) {
+//       _showAlertDialog(context, 'Invalid email format.');
+//       return;
+//     }
+//
+//     if (enteredEmail != hardcodedEmail || enteredPassword != hardcodedPassword) {
+//       _showAlertDialog(context, 'Incorrect email or password.');
+//       return;
+//     }
+//
+//     // Navigate to the faculty home page after successful login
+//     Navigator.pushReplacement(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) {
+//           // ignore: prefer_const_constructors
+//           return FacultyHomePage(facultyName: 'Faculty');
+//         }, // Pass faculty's name here
+//       ),
+//     );
+//   }
+//
+//
+//   void _showAlertDialog(BuildContext context, String message) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text('Login Failed'),
+//           content: Text(message),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.pop(context);
+//               },
+//               child: const Text('OK'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
