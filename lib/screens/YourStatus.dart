4de319172cf363f3729727627c5e3273e34c2995
om1007 <117250624+om1007@users.noreply.github.com>
2024-05-11 20:31:50 +0530

    Update YourStatus.dart

--- lib/screens/YourStatus.dart
+++ lib/screens/YourStatus.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class YourStatus extends StatefulWidget {
   const YourStatus({Key? key}) : super(key: key);
@@ -9,12 +11,100 @@ class YourStatus extends StatefulWidget {
 }
 
 class _YourStatusState extends State<YourStatus> {
+  late User? _currentUser;
+
   bool _isAvailable = false;
   TimeOfDay _startTime = TimeOfDay(hour: 9, minute: 0);
   TimeOfDay _endTime = TimeOfDay(hour: 17, minute: 0);
   String _location = '';
   String _phoneNumber = '';
   String _email = '';
+
+  Future<void> _getCurrentUser() async {
+    _currentUser = FirebaseAuth.instance.currentUser;
+    setState(() {});
+  }
+
+  Future<void> _saveChanges(BuildContext context) async {
+    try {
+      if (_currentUser == null) {
+        throw Exception('User not authenticated.');
+      }
+
+      final facultyRef = FirebaseFirestore.instance.collection('faculties').doc(_currentUser!.uid);
+
+      final data = {
+        'displayName': _currentUser!.displayName,
+        'available': _isAvailable,
+        'startTime': _startTime.format(context),
+        'endTime': _endTime.format(context),
+        'location': _location,
+        'phoneNumber': _phoneNumber,
+        'email': _email,
+      };
+
+      await facultyRef.set(data);
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Changes Saved'),
+            content: Text('Your status has been updated successfully.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (error) {
+      print("Error saving changes: $error");
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Failed to save changes. Please try again.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context, bool isStartTime) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: isStartTime ? _startTime : _endTime,
+    );
+    if (picked != null) {
+      setState(() {
+        if (isStartTime) {
+          _startTime = picked;
+        } else {
+          _endTime = picked;
+        }
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUser();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,203 +117,101 @@ class _YourStatusState extends State<YourStatus> {
         title: Text('Set Your Status'),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Current Date: $formattedDate',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 12.0), // Adjusted line spacing
-              Text(
-                'Current Time: $formattedTime',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              Text(
-                'Faculty Name:', // Replace with actual faculty name
-                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                'Prof. Arjun Jaiswal', // Replace with actual faculty name
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              Row(
-                children: [
-                  Text(
-                    'Availability:',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  SizedBox(width: 8.0),
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        _isAvailable = !_isAvailable;
-                      });
-                    },
-                    child: Container(
-                      width: 30.0,
-                      height: 30.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: _isAvailable ? Colors.green : Colors.red,
-                      ),
-                      child: Icon(
-                        _isAvailable ? Icons.check : Icons.close,
-                        color: Colors.white,
-                      ),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Current Date: $formattedDate', style: TextStyle(fontSize: 16.0)),
+            SizedBox(height: 12.0),
+            Text('Current Time: $formattedTime', style: TextStyle(fontSize: 16.0)),
+            SizedBox(height: 24.0),
+            Text('Faculty Name:', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+            SizedBox(height: 8.0),
+            Text(_currentUser?.displayName ?? 'Loading...', style: TextStyle(fontSize: 16.0)),
+            SizedBox(height: 24.0),
+            Row(
+              children: [
+                Text('Availability:', style: TextStyle(fontSize: 16.0)),
+                SizedBox(width: 8.0),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isAvailable = !_isAvailable;
+                    });
+                  },
+                  child: Container(
+                    width: 30.0,
+                    height: 30.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _isAvailable ? Colors.green : Colors.red,
+                    ),
+                    child: Icon(
+                      _isAvailable ? Icons.check : Icons.close,
+                      color: Colors.white,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              Text(
-                'Timings:\n',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              Row(
-                children: [
-                  Text(
-                    'From:',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  SizedBox(width: 8.0),
-                  InkWell(
-                    onTap: () {
-                      _selectStartTime(context);
-                    },
-                    child: Text(
-                      _startTime.format(context),
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ),
-                  SizedBox(width: 16.0),
-                  Text(
-                    'To:',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  SizedBox(width: 8.0),
-                  InkWell(
-                    onTap: () {
-                      _selectEndTime(context);
-                    },
-                    child: Text(
-                      _endTime.format(context),
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              Text(
-                'Location:',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    _location = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  hintText: 'Enter location',
                 ),
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              Text(
-                'Phone Number:',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    _phoneNumber = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  hintText: 'Enter phone number',
+              ],
+            ),
+            SizedBox(height: 24.0),
+            Row(
+              children: [
+                Text('From:', style: TextStyle(fontSize: 16.0)),
+                SizedBox(width: 8.0),
+                GestureDetector(
+                  onTap: () => _selectTime(context, true),
+                  child: Text(_startTime.format(context), style: TextStyle(fontSize: 16.0)),
                 ),
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              Text(
-                'Email:',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  hintText: 'Enter email',
+                SizedBox(width: 16.0),
+                Text('To:', style: TextStyle(fontSize: 16.0)),
+                SizedBox(width: 8.0),
+                GestureDetector(
+                  onTap: () => _selectTime(context, false),
+                  child: Text(_endTime.format(context), style: TextStyle(fontSize: 16.0)),
                 ),
-              ),
-              SizedBox(height: 24.0), // Adjusted line spacing
-              ElevatedButton(
-                onPressed: () {
-                  // Save changes logic
-                  _saveChanges(context);
-                },
-                child: Text('Save Changes'),
-              ),
-            ],
-          ),
+              ],
+            ),
+            SizedBox(height: 24.0),
+            Text('Location:', style: TextStyle(fontSize: 16.0)),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  _location = value;
+                });
+              },
+              decoration: InputDecoration(hintText: 'Enter location'),
+            ),
+            SizedBox(height: 24.0),
+            Text('Phone Number:', style: TextStyle(fontSize: 16.0)),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  _phoneNumber = value;
+                });
+              },
+              decoration: InputDecoration(hintText: 'Enter phone number'),
+            ),
+            SizedBox(height: 24.0),
+            Text('Email:', style: TextStyle(fontSize: 16.0)),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  _email = value;
+                });
+              },
+              decoration: InputDecoration(hintText: 'Enter email'),
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () {
+                _saveChanges(context);
+              },
+              child: Text('Save Changes'),
+            ),
+          ],
         ),
       ),
     );
-
   }
-
-  Future<void> _selectStartTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _startTime,
-    );
-    if (picked != null && picked != _startTime) {
-      setState(() {
-        _startTime = picked;
-      });
-    }
-  }
-
-  Future<void> _selectEndTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _endTime,
-    );
-    if (picked != null && picked != _endTime) {
-      setState(() {
-        _endTime = picked;
-      });
-    }
-  }
-}
-void _saveChanges(BuildContext context) {
-  // Add your save logic here
-
-  // Show alert dialog after saving changes
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Changes Saved'),
-        content: Text('Your status has been updated successfully.'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              // Navigate back to StatusNavigator.dart
-              Navigator.pop(context); // Close the dialog
-              Navigator.pop(context); // Navigate back to StatusNavigator.dart
-            },
-            child: Text('OK'),
-          ),
-        ],
-      );
-    },
-  );
 }
